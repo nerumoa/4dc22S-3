592f7386b4c01d1981a18bf5c4d1bceef8632021
Ryuga5 <R.e123@icloud.com>
2022-08-10 13:18:51 +0900

    add
    Timerの作成

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    private TextMeshProUGUI timerText;
+    private float second;
+    private int minute;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timerText = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        second += Time.deltaTime;
+
+        if (second > 60f)
+        {
+            minute += 1;
+            second = 0;
+        }
+        timerText.text = minute.ToString("00") + ":" + second.ToString("f2");
+    }
+}
