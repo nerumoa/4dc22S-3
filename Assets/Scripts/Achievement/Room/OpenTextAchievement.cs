99fed3e59aa4207209ede0d8dd0a86765175f388
refsonman <calpiso30@gmail.com>
2022-08-12 03:04:05 +0900

    add achievement sentence

--- Assets/Scripts/Achievement/Room/OpenTextAchievement.cs
+++ Assets/Scripts/Achievement/Room/OpenTextAchievement.cs
@@ -6,92 +6,92 @@ public class OpenTextAchievement : MonoBehaviour
 {
     public string GetOpenText1()
     {
-        return "餅つきを10回成功させた。";
+        return "餅つきを10回成功させた。\n\nもう初心者とは言わせない。";
     }
 
     public string GetOpenText2()
     {
-        return "餅つきを100回成功させた。";
+        return "餅つきを100回成功させた。\n\nのどに詰まらせないように。";
     }
 
     public string GetOpenText3()
     {
-        return "餅つきを1000回成功させた。";
+        return "餅つきを1000回成功させた。\n\n世界大会への準備は整ったか？";
     }
 
     public string GetOpenText4()
     {
-        return "餅つきを5回失敗した。";
+        return "餅つきを5回失敗した。\n\n良い素材を見つけたわね";
     }
 
     public string GetOpenText5()
     {
-        return "餅つきを50回失敗した。";
+        return "餅つきを50回失敗した。\n\n全然まだまだノーダメージ";
     }
 
     public string GetOpenText6()
     {
-        return "餅つきを100回失敗した。";
+        return "餅つきを100回失敗した。\n\nまだいけますよね？";
     }
 
     public string GetOpenText7()
     {
-        return "ゲームオーバー回数が1回になった。";
+        return "ゲームオーバー回数が1回になった。\n\nサンキューベリー餅";
     }
 
     public string GetOpenText8()
     {
-        return "ゲームオーバー回数が5回になった。";
+        return "ゲームオーバー回数が5回になった。\n\nもうちょっとやっていきなよ";
     }
 
     public string GetOpenText9()
     {
-        return "ゲームオーバー回数が10回になった。";
+        return "ゲームオーバー回数が10回になった。\n\nやっぱりそろそろやめにしないか？";
     }
 
     public string GetOpenText10()
     {
-        return "一回のゲームのスコア量が1万を超えた。";
+        return "一回のゲームのスコア量が1万を超えた。\n\nさぁてみなさ～ん、餅つきの時間でぇす";
     }
 
     public string GetOpenText11()
     {
-        return "一回のゲームのスコア量が5万を超えた。";
+        return "一回のゲームのスコア量が5万を超えた。\n\nひょっとしてもう飽きてない？";
     }
 
     public string GetOpenText12()
     {
-        return "一回のゲームのスコア量が10万を超えた。";
+        return "一回のゲームのスコア量が10万を超えた。\n\n至高の領域に近い。お前も餅にならないか？";
     }
 
     public string GetOpenText13()
     {
-        return "今までの合計スコア量が20万を超えた。";
+        return "今までの合計スコア量が20万を超えた。\n\nまだまだ序の口ですよ";
     }
 
     public string GetOpenText14()
     {
-        return "今までの合計スコア量が30万を超えた。";
+        return "今までの合計スコア量が30万を超えた。\n\n腹に溜まるからあんまり食えない";
     }
 
     public string GetOpenText15()
     {
-        return "今までの合計スコア量が50万を超えた。";
+        return "今までの合計スコア量が50万を超えた。\n\nさらに向こうへ。Plus Ultra";
     }
 
     public string GetOpenText16()
     {
-        return "起動回数が2回になった。";
+        return "起動回数が2回になった。\n\n何回でもプレイしていってね";
     }
 
     public string GetOpenText17()
     {
-        return "起動回数が3回になった。";
+        return "起動回数が3回になった。\n\nもう立派なリピーターです。";
     }
 
     public string GetOpenText18()
     {
-        return "起動回数が4回になった。";
+        return "起動回数が4回になった。\n\nもはやルーティン";
     }
 
     public string GetOpenText19()
