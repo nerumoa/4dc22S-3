8217e54ec6bd2738289f55b3c97a83f3db4c8800
refsonman <calpiso30@gmail.com>
2022-08-12 02:30:59 +0900

    実績説明文書いた

--- Assets/Scripts/Achievement/Room/OpenTextAchievement.cs
+++ Assets/Scripts/Achievement/Room/OpenTextAchievement.cs
@@ -6,121 +6,121 @@ public class OpenTextAchievement : MonoBehaviour
 {
     public string GetOpenText1()
     {
-        return "餅を搗きし者";
+        return "餅つきを10回成功させた。";
     }
 
     public string GetOpenText2()
     {
-        return "お餅大好き";
+        return "餅つきを100回成功させた。";
     }
 
     public string GetOpenText3()
     {
-        return "ウルトラ餅ニスト";
+        return "餅つきを1000回成功させた。";
     }
 
     public string GetOpenText4()
     {
-        return "Welcome Hell";
+        return "餅つきを5回失敗した。";
     }
 
     public string GetOpenText5()
     {
-        return "餅搗きに失敗は搗きもの";
+        return "餅つきを50回失敗した。";
     }
 
     public string GetOpenText6()
     {
-        return "あきらめるな、まだ搗ける";
+        return "餅つきを100回失敗した。";
     }
 
     public string GetOpenText7()
     {
-        return "ゲームプレイ、感謝します";
+        return "ゲームオーバー回数が1回になった。";
     }
 
     public string GetOpenText8()
     {
-        return "お疲れ様です";
+        return "ゲームオーバー回数が5回になった。";
     }
 
     public string GetOpenText9()
     {
-        return "まだやるの？";
+        return "ゲームオーバー回数が10回になった。";
     }
 
     public string GetOpenText10()
     {
-        return "すばらしい！";
+        return "一回のゲームのスコア量が1万を超えた。";
     }
 
     public string GetOpenText11()
     {
-        return "とてもすばらしい！";
+        return "一回のゲームのスコア量が5万を超えた。";
     }
 
     public string GetOpenText12()
     {
-        return "すばらしい・オブザイヤー";
+        return "一回のゲームのスコア量が10万を超えた。";
     }
 
     public string GetOpenText13()
     {
-        return "よくやった！";
+        return "今までの合計スコア量が20万を超えた。";
     }
 
     public string GetOpenText14()
     {
-        return "餅も積もれば山となる";
+        return "今までの合計スコア量が30万を超えた。";
     }
 
     public string GetOpenText15()
     {
-        return "千里の餅も、ひと足宛はこぶなり";
+        return "今までの合計スコア量が50万を超えた。";
     }
 
     public string GetOpenText16()
     {
-        return "おかえり";
+        return "起動回数が2回になった。";
     }
 
     public string GetOpenText17()
     {
-        return "待ってた";
+        return "起動回数が3回になった。";
     }
 
     public string GetOpenText18()
     {
-        return "今日も一日";
+        return "起動回数が4回になった。";
     }
 
     public string GetOpenText19()
     {
-        return "ラッキーマン";
+        return "レア鏡餅を発見した。";
     }
 
     public string GetOpenText20()
     {
-        return "重課金者";
+        return "スーパーレア鏡餅を発見した。";
     }
 
     public string GetOpenText21()
     {
-        return "出席確認";
+        return "クレジットを見に行った。";
     }
 
     public string GetOpenText22()
     {
-        return "兎の耳に神曲";
+        return "ミュージックルームですべての曲を聴いた。";
     }
 
     public string GetOpenText23()
     {
-        return "虚無";
+        return "タイトル画面で5分放置した。";
     }
 
     public string GetOpenText24()
     {
-        return "ガンジー";
+        return "一回も餅をつかずにゲームオーバーになった。";
     }
 }
