60d80745fa2037dec19dae4796799618a31015c1
nerumoa <50538677+nerumoa@users.noreply.github.com>
2022-08-11 20:47:09 +0900

    csファイルの追加

---
+++ Assets/Scripts/Achievement/Room/NotOpenAchievement.cs
@@ -0,0 +1,126 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotOpenAchievement : MonoBehaviour
+{
+    public string GetText1()
+    {
+        return "おかえり";
+    }
+
+    public string GetText2()
+    {
+        return "待ってた";
+    }
+
+    public string GetText3()
+    {
+        return "今日も一日";
+    }
+
+    public string GetText4()
+    {
+        return "ゲームプレイ、\n感謝します";
+    }
+
+    public string GetText5()
+    {
+        return "お疲れ様です";
+    }
+
+    public string GetText6()
+    {
+        return "まだやるの？";
+    }
+
+    public string GetText7()
+    {
+        return "すばらしい！";
+    }
+
+    public string GetText8()
+    {
+        return "とても\nすばらしい！";
+    }
+
+    public string GetText9()
+    {
+        return "すばらしい・\nオブザイヤー";
+    }
+
+    public string GetText10()
+    {
+        return "よくやった！";
+    }
+
+    public string GetText11()
+    {
+        return "餅も積もれば\n山となる";
+    }
+
+    public string GetText12()
+    {
+        return "千里の餅も、\nひと足宛はこぶなり";
+    }
+
+    public string GetText13()
+    {
+        return "餅を搗きし者";
+    }
+
+    public string GetText14()
+    {
+        return "お餅大好き";
+    }
+
+    public string GetText15()
+    {
+        return "ウルトラ餅ニスト";
+    }
+
+    public string GetText16()
+    {
+        return "Welcome Hell";
+    }
+
+    public string GetText17()
+    {
+        return "餅搗きに\n失敗は搗きもの";
+    }
+
+    public string GetText18()
+    {
+        return "あきらめるな、\nまだ搗ける";
+    }
+
+    public string GetText19()
+    {
+        return "ラッキーマン";
+    }
+
+    public string GetText20()
+    {
+        return "重課金者";
+    }
+
+    public string GetText21()
+    {
+        return "出席確認";
+    }
+
+    public string GetText22()
+    {
+        return "兎の耳に神曲";
+    }
+
+    public string GetText23()
+    {
+        return "虚無";
+    }
+
+    public string GetText24()
+    {
+        return "ガンジー";
+    }
+}
